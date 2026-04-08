dec4bda1f53aaa7f7763254d070f83104f90815d
Zihni AKIN <160263157+1akin1@users.noreply.github.com>
2026-04-08 19:41:30 +0300

    Delete GuitarStore directory

--- GuitarStore/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace GuitarStore.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
